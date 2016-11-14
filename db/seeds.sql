cb3e9a00b800665a04901aa5f85aff4c2e29ce1f
clshu <ccss0630@yahoo.com>
2016-11-14 01:11:40 -0600

    sequelize init and remove files specific to npm mysql and Week14-HW12-Burger

--- db/seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO burgers (burger_name) VALUES ('Chichen Tender Burger');
-INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cajun Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Seafood Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pork Cheese Burer', true);
-INSERT INTO burgers (burger_name) VALUES ('Double Deck Cheese Burger');
